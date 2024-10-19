e453882672bd685a0671268b4bc0505aa2ad0762
qqqyt2000 <88172368+qqqyt2000@users.noreply.github.com>
2024-10-19 12:06:18 +0800

    Add files via upload

---
+++ QiuYuting_Assignment2_PyGameTry_penguins & pizza/settings.py
@@ -0,0 +1,9 @@
+class Settings():
+    
+    def __init__(self):
+        '''屏幕设置'''
+        self.screen_width = 900
+        self.screen_height = 600
+        self.bg_color = (230,230,230)#浅灰色
+        self.pizza_limit = 10
+        self.pizza_points = 1
